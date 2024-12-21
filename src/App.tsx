c3730788ab03d35aa812471f9ab71e34c43ce888
Aokemi <audrin.yann@gmail.com>
2024-12-21 16:43:59 +0100

    Implemented header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/Theme";
+import GlobalStyle from "./styles/GlobalStyle";
+import HeaderComponent from "./Components/Header/Header";
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+            <div>
+              <HeaderComponent />
+            </div>
+      </>
+    </ThemeProvider>
   )
 }
 
